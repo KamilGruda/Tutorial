5a2ef82946f7e72bc66ca83a911896850a777e96
Kamil Gruda <gruda2008@gmail.com>
2018-12-16 21:27:42 +0100

    First commit in IDE

--- src/pl/kamilgruda/udemy/firstprogram/Main.java
+++ src/pl/kamilgruda/udemy/firstprogram/Main.java
@@ -3,5 +3,6 @@ package pl.kamilgruda.udemy.firstprogram;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //Commit from IDE
     }
 }
